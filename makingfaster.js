bdca595d34574b00956b1809470fa4b7fbf95d6d
Ajay_Bains <ajay.bains@codvo.ai>
2023-04-03 09:26:08 +0530

    memoized with callback

--- makingfaster.js
+++ makingfaster.js
@@ -1,6 +1,6 @@
 // unique arr
 
-const isUnique = (arr) => {
+/* const isUnique = (arr) => {
   let result = true;
 
   for (let i = 0; i < arr.length; i++) {
@@ -15,15 +15,15 @@ const isUnique = (arr) => {
   }
 
   return result;
-};
+}; */
 
 //   console.log(isUnique([1,2,3]) === true
 
 // console.log(isUnique([3,4,2,5]))
 
-// making above algo faster
+// making above algo faster*////////////////////////////////////////////////////////////
 
-const isUniqueBreadCrumbs = (arr) => {
+/* const isUniqueBreadCrumbs = (arr) => {
   let breadcrumbs = {};
   let result = true;
   for (let i = 0; i < arr.length; i++) {
@@ -35,7 +35,7 @@ const isUniqueBreadCrumbs = (arr) => {
   }
 
   return result;
-};
+}; */
 
 // console.log(isUniqueBreadCrumbs([1, 2, 3, 4, 5]));
 
@@ -47,7 +47,7 @@ const isUniqueBreadCrumbs = (arr) => {
 //input: [1,5,2,1] => output: [1,2,5]
 //input: [4,2,2,3,2,2,2] => output: [2,3,4]
 
-const uniqSort = function (arr) {
+/* const uniqSort = function (arr) {
   const breadcrumbs = {};
   let uniqArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -59,7 +59,7 @@ const uniqSort = function (arr) {
   }
 
   return uniqArr.sort((a, b) => a - b);
-};
+}; */
 
 // console.log(uniqSort([4, 2, 2, 3, 2, 2, 2,1])); // => [2,3,4]
 
@@ -98,12 +98,12 @@ const uniqSort = function (arr) {
 
 // ************Memoization with closure*******
 
-const times10 = (n) => n * 10;
+/* const times10 = (n) => n * 10;
 
 // Task 3: Clean up your global scope by moving your cache inside your function.
 // protip: Use a closure to return a function that you can call later.
 
-const memoizedClosureTimes10 = (n) => {
+const memoizedClosureTimes10 = () => {
   let cache={};
   return (n)=>{
     if(n in cache){
@@ -127,4 +127,38 @@ try {
   console.log("Task 3 cached value:", memoClosureTimes10(9)); // cached
 } catch (e) {
   console.error("Task 3:", e);
+}
+ */
+
+/****************************************************************************************************** */
+
+// task 4
+
+const times10 = (n) => n * 10;
+// Task 4: Make your memo function generic and accept the times10 function as a callback rather than defining the n * 10 logic inside the if/else or pulling it in from the global scope.
+
+// protip: Take advantage of the fact that parameters are saved in the closure as well, just like the cache from the previous example.
+const memoize = (cb) => {
+  let cache = {};
+  return (n) => {
+    if (n in cache) {
+      console.log("fetching from cche");
+      return cache[n]
+    } else {
+      console.log("calculating result");
+      let result = cb(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+};
+
+// returned function from memoizedAdd
+const memoizedTimes10 = memoize(times10);
+console.log("~~~~~~~~~~~~~~TASK 4~~~~~~~~~~~~~~");
+try {
+  console.log("Task 4 calculated value:", memoizedTimes10(9)); // calculated
+  console.log("Task 4 cached value:", memoizedTimes10(9)); // cached
+} catch (e) {
+  console.error("Task 4:", e);
 }
